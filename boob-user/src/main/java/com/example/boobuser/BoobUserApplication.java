8c3e64a3dbae81f107f7ee2c688e9ca8ff3f61f3
JdtVia <3107522713@qq.com>
2023-12-19 13:27:53 +0800

    增加数据库插入数据项

--- boob-user/src/main/java/com/example/boobuser/BoobUserApplication.java
+++ boob-user/src/main/java/com/example/boobuser/BoobUserApplication.java
@@ -1,9 +1,15 @@
 package com.example.boobuser;
 
+import com.example.boobuser.mapper.PermissionDAO;
+import com.example.boobuser.mapper.RoleDAO;
+import com.example.boobuser.model.Permission;
+import com.example.boobuser.model.Role;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,7 +17,19 @@ import org.springframework.context.annotation.ComponentScan;
 public class BoobUserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BoobUserApplication.class, args);
+        var ctx=SpringApplication.run(BoobUserApplication.class, args);
+
+        System.out.println("插入");
+        var role=ctx.getBean(RoleDAO.class);
+        var permission=ctx.getBean(PermissionDAO.class);
+        var permission1=new Permission(1,"删除自己帖子",List.of());
+        var permission2=new Permission(2,"删除别人帖子",List.of());
+        permission.save(permission1);
+        permission.save(permission2);
+        role.save(new Role(1,"普通用户", List.of(permission1)));
+        role.save(new Role(2,"管理员", List.of(permission1,permission2)));
+        System.out.println("完成");
+
     }
 
 }
